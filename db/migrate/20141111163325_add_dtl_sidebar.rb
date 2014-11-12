a3326f7ffe1d7baa5338384c67096e1913fdc418
Jingchao Lee <satzcoal@hotmail.com>
2014-11-12 12:28:13 +0800

    tiny_edit

--- db/migrate/20141111163325_add_dtl_sidebar.rb
+++ db/migrate/20141111163325_add_dtl_sidebar.rb
@@ -1,6 +1,9 @@
 #encoding: utf-8
 class AddDtlSidebar < ActiveRecord::Migration
   def up
+    item = Origin::SidebarItem.find_by_name('资料管理')
+    item.destroy if item
+
     item = Origin::SidebarItem.find_or_create_by(:name => '客户管理')
     item.url = '#'
     item.site_id = 1
