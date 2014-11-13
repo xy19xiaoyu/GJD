72b9d4e3a31f84f8aa994fc8ba5c4aaa4cbeda62
李靖超 <satzcoal@hotmail.com>
2014-11-13 11:37:43 +0800

    Delete 20141110193855_delete_lower_id_from_customer.rb

--- db/migrate/20141110193855_delete_lower_id_from_customer.rb
+++
@@ -1,9 +0,0 @@
-class DeleteLowerIdFromCustomer < ActiveRecord::Migration
-  def up
-    remove_column :info_customers, :lower_id
-  end
-
-  def down
-    add_column :info_customers, :lower_id, :integer
-  end
-end
